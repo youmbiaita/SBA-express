cb2d610c2aa5250cca1f5fbeb43d75db4b240464
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-05-23 18:48:45 -0400

    added changes

---
+++ data/menu.js
@@ -0,0 +1,64 @@
+const menu = [
+    {
+        id: 1,
+        name: 'Ndole',
+        description: 'Fresh betterleaf with peanurs and cow meat, dry fish',
+        price: 24.99,
+        category: 'Main'
+    },
+    {
+        id: 2,
+        name: 'Eru',
+        description: 'leaves wirh palm oil and dry meats',
+        price: 24.99,
+        category: 'Main'
+    },
+    {
+        id: 3,
+        name: 'DG',
+        description: 'Fresh green plantains and veggies',
+        price: 6.99,
+        category: 'main'
+    },
+    {
+        id: 4,
+        name: 'Koki',
+        description: 'blackeye beans',
+        price: 6.99, category: 'main'
+    },
+    {
+        id: 5,
+        name: 'Grilled fish',
+        description: 'charcoal grilled fish with spices ',
+        price: 6.99, category: 'main'
+    },
+    {
+        id: 6,
+        name: 'Salad',
+        description: 'Fresh greens and veggies',
+        price: 6.99, category: 'Starter'
+    },
+    {
+        id: 7,
+        name: 'puff puff',
+        description: 'flour and sugar',
+        price: 6.99, category: 'Starter'
+    },
+    {
+        id: 8,
+        name: 'fried plantains',
+        description: 'fried plantaims',
+        price: 6.99,
+        category: 'side'
+    },
+    {
+        id: 9,
+        name: 'Bobolo, foufou',
+        description: 'side ',
+        price: 6.99,
+        category: 'side'
+    },
+
+
+
+]

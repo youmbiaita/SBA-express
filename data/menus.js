333ebf860fa1d20d358c5566f806fc980e9e66f8
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-05-23 22:25:58 -0400

    added some changes

--- data/menus.js
+++ data/menus.js
@@ -1,64 +1,66 @@
-const menu = [
+const menus = [
     {
         id: 1,
         name: 'Ndole',
         description: 'Fresh betterleaf with peanurs and cow meat, dry fish',
-        price: 24.99,
-        category: 'Main'
+        price: 20.99
     },
     {
         id: 2,
         name: 'Eru',
         description: 'leaves wirh palm oil and dry meats',
-        price: 24.99,
-        category: 'Main'
+        price: 20.99
     },
     {
         id: 3,
         name: 'DG',
         description: 'Fresh green plantains and veggies',
-        price: 6.99,
-        category: 'main'
+        price: 19.99
     },
     {
         id: 4,
         name: 'Koki',
         description: 'blackeye beans',
-        price: 6.99, category: 'main'
+        price: 17.98
     },
     {
         id: 5,
         name: 'Grilled fish',
         description: 'charcoal grilled fish with spices ',
-        price: 6.99, category: 'main'
+        price: 19.98
     },
     {
         id: 6,
         name: 'Salad',
         description: 'Fresh greens and veggies',
-        price: 6.99, category: 'Starter'
+        price: 6.99
     },
     {
         id: 7,
         name: 'puff puff',
         description: 'flour and sugar',
-        price: 6.99, category: 'Starter'
+        price: 6.99
     },
     {
         id: 8,
-        name: 'fried plantains',
-        description: 'fried plantaims',
-        price: 6.99,
-        category: 'side'
+        name: 'fried plantains or boiled plantain',
+        description: 'fried plantains',
+        price: 3.99
     },
     {
         id: 9,
-        name: 'Bobolo, foufou',
-        description: 'side ',
-        price: 6.99,
-        category: 'side'
+        name: 'Bobolo',
+        description: 'match cassava ',
+        price: 3.99
     },
+    {
+        id: 10,
+        name: ' foufou',
+        description: 'cassava ',
+        price: 3.99
+    }
 
 
 
-]
+];
+module.exports = menus;
---
+++ data/orders.js
@@ -0,0 +1,46 @@
+const orders = [
+    {
+        id: 1,
+        userId: 1,
+        items: [1, 8],
+        total: 24.98,
+        status: 'pending'
+    },
+    {
+        id: 2,
+        userId: 2,
+        items: [3, 10],
+         total: 24.98,
+        status: 'completed'
+    },
+    {
+        id: 3,
+        userId: 3,
+        items: [3],
+         total: 19.99,
+        status: 'completed'
+    },
+    {
+        id: 4,
+        userId: 4,
+        items: [4,8],
+         total: 21.98,
+        status: 'pending'
+    },
+    {
+        id: 5,
+        userId: 5,
+        items: [5,6,9],
+         total: 30.97,
+        status: 'pending'
+    },
+    {
+        id: 6,
+        userId: 7,
+        items: [7],
+         total: 6.99,
+        status: 'completed'
+    }
+
+];
+module.exports = orders;
